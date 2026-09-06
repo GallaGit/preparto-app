9a5b37f850f0b2c7e1b7399171f78ae573cb5cb8
GallaGit <GallaGit@users.noreply.github.com>
2026-09-06 19:36:42 +0000

    deploy: actualizar GitHub Pages 1baf846a333b67502996f000b8beda93b54cd72e

---
+++ assets/index-FNna8ucY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BP2QUNvY.js","assets/index-DXoDgWuS.js"])))=>i.map(i=>d[i]);
+import{_ as r}from"./index-BMdvkXPS.js";import{registerPlugin as o}from"./index-DXoDgWuS.js";const i=o("SplashScreen",{web:()=>r(()=>import("./web-BP2QUNvY.js"),__vite__mapDeps([0,1])).then(e=>new e.SplashScreenWeb)});export{i as SplashScreen};
